5bbda7689ec356231d997279e7b1e375d117d14b
YOLOswagger6669 <samosley@gmail.com>
2014-11-19 18:46:27 -0700

    Pointing.javaleena
    I got an error on lines 23 and 37 of PointMain saying "distance(int,int) in Point cannot be applied to (Point)". Something is wrong with my distance method but I have no idea what.

--- src/Point.java
+++ src/Point.java
@@ -1,2 +1,43 @@
 // A Point object represents a pair of (x, y) integer coordinates.
 // Reference Building Java Programs pp 505-517
+import java.lang.*;
+public class Point
+{
+  int x;
+  int y;
+  public int getX()
+  {
+    return x;
+  }
+  public int getY()
+  {
+    return y;
+  }
+  public String toString()
+  {
+    String a=(Integer.toString(x)+", "+Integer.toString(y));
+    return a;
+  }
+  public double distanceFromOrigin()
+  {
+    double a=x*x+y*y;
+    return Math.sqrt(a);
+  }
+  public double distance (int t, int z)
+  {
+    return Math.sqrt((x-t)*(x-t)+(y-z)*(y-z));
+  }
+  public void setX (int a)
+  {
+    x=a;
+  }
+  public void setY (int a)
+  {
+    y=a;
+  }
+  public void translate (int t, int z)
+  {
+    x+=t;
+    y+=z;
+  }
+}
